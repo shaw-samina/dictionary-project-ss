94b7eb90d313a1b86c53dd87712172d121c89805
shaw-samina <jinxcapsule@yahoo.com>
2026-07-06 22:24:39 -0400

    initial commit

---
+++ dictionary-project-ss/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+
+export default function App() {
+  return (
+    <div>
+      <div>hello</div>
+      <button type="button" className="btn btn-primary">
+        go
+      </button>
+    </div>
+  );
+}
